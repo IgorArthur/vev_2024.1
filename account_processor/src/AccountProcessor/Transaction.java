f46f4683b9e7ea8b85a7c5a7ed70d4fad352fb64
IgorArthur <igorart1210@gmail.com>
2024-07-31 20:31:17 -0300

    AP - Creates Transaction class and tests

---
+++ account_processor/src/AccountProcessor/Transaction.java
@@ -0,0 +1,32 @@
+package AccountProcessor;
+
+import java.time.LocalDate;
+
+public class Transaction {
+
+  private String id;
+  private LocalDate date;
+  private double value;
+
+  public Transaction(String id, LocalDate date, double value) {
+    this.id = id;
+    this.date = date;
+    this.value = value;
+  }
+
+  public boolean equals(Transaction transaction) {
+    if (this == transaction)
+      return true;
+    if (transaction == null)
+      return false;
+    if (!(transaction instanceof Transaction))
+      return false;
+    Transaction other = (Transaction) transaction;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+}
