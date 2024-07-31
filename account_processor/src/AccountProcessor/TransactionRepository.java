d867aef33b30b2eea37a7f86d388e46238fe8e9e
IgorArthur <igorart1210@gmail.com>
2024-07-31 20:41:01 -0300

    AP - Creates TransactionRepository class and tests

---
+++ account_processor/src/AccountProcessor/TransactionRepository.java
@@ -0,0 +1,21 @@
+package AccountProcessor;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+
+public class TransactionRepository {
+  private HashMap<String, Transaction> transactionMap = new HashMap<>();
+
+  public boolean existsTransaction(String id) {
+    return this.transactionMap.containsKey(id);
+  }
+
+  public Transaction geTransaction(String id) {
+    return this.transactionMap.get(id);
+  }
+
+  public void registerTransaction(String id, LocalDate date, double value) {
+    Transaction transaction = new Transaction(id, date, value);
+    transactionMap.put(id, transaction);
+  }
+}
