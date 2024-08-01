ac027a80fed62b8adc896a340030e76b3749d68b
IgorArthur <igorart1210@gmail.com>
2024-07-31 21:46:36 -0300

    AP - Adds register Transaction and Invoice methods and tests

--- account_processor/src/AccountProcessor/Controller.java
+++ account_processor/src/AccountProcessor/Controller.java
@@ -1,5 +1,35 @@
 package AccountProcessor;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+
 public class Controller {
-  
+
+  private TransactionRepository transactionRepository = new TransactionRepository();
+  private InvoiceRepository invoiceRepository = new InvoiceRepository();
+
+  public String registerTransaction(String id, LocalDate date, double value) {
+    if (id.equals("") || date == null || value == 0) {
+      throw new IllegalArgumentException("ENTRADA INVÁLIDA!");
+    }
+    if (transactionRepository.existsTransaction(id)) {
+      return "CONTA JÁ CADASTRADA!";
+    }
+
+    transactionRepository.registerTransaction(id, date, value);
+    return "CONTA CADASTRADA!";
+  }
+
+  public String registerInvoice(String id, LocalDate date, double value, String clientName,
+      HashSet<Transaction> transactions) {
+    if (id.equals("") || date == null || value == 0 || clientName.equals("")) {
+      throw new IllegalArgumentException("ENTRADA INVÁLIDA!");
+    }
+    if (invoiceRepository.existsInvoice(id)) {
+      return "FATURA JÁ CADASTRADA!";
+    }
+
+    invoiceRepository.registerInvoice(id, date, value, clientName, transactions);
+    return "FATURA CADASTRADA!";
+  }
 }
--- account_processor/src/Test/ControllerTest.java
+++ account_processor/src/Test/ControllerTest.java
@@ -1,10 +1,30 @@
 package Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+
 import org.junit.Test;
 
+import AccountProcessor.Controller;
+import AccountProcessor.Transaction;
+
 public class ControllerTest {
+
+  Controller controller = new Controller();
+
   @Test
-  public void firstTest() {
-    Controller controller = new Controller();
+  public void testTransactionExists() {
+    controller.registerTransaction("001", LocalDate.parse("2024-06-20"), 15.00);
+
+    assertEquals("CONTA JÁ CADASTRADA!", controller.registerTransaction("001", LocalDate.parse("2024-06-20"), 15.00));
+  }
+
+  @Test
+  public void testInvoiceExists() {
+    controller.registerInvoice("001", LocalDate.parse("2024-06-20"), 15.00, "Cliente Teste", new HashSet<Transaction>());
+
+    assertEquals("FATURA JÁ CADASTRADA!", controller.registerInvoice("001", LocalDate.parse("2024-06-20"), 15.00, "Cliente Teste", new HashSet<Transaction>()));
   }
 }
