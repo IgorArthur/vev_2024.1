f2576d0bc2ef9fa892a4cb6b60cca1b008e3cc7c
LuizCarlosXavier <62178110+LuizCarlosXavier@users.noreply.github.com>
2024-08-17 17:14:48 -0300

    TS: Refatoração completa de Lote.java e correção da lógica de venda e cadastro de ingressos

--- src/main/java/entities/Lote.java
+++ src/main/java/entities/Lote.java
@@ -3,57 +3,151 @@ package entities;
 import java.util.ArrayList;
 import java.util.List;
 
+import entities_enum.Status;
+import entities_enum.TipoDeIngresso;
+
 public class Lote {
     private String id;
     private List<Ingresso> ingressos = new ArrayList<>();
-    private Double desconto;
+    private Boolean elegivelDesconto;
+    Double maxDesconto;
+    Double maxIngressosVip;
+    double ingressos_meia;
+    double ingressos_vips;
+    double ingressos_normais;
 
-    
-    
-    public Lote(String id, double desconto) {
-        // Garantir que o desconto não exceda 25%
-        this.desconto = Math.min(desconto, 0.25);
+
+    public Lote(String id) {
         this.id = id;
+        this.maxDesconto = 25.0;
+        this.maxIngressosVip  = 30.0; // %
     }
 
-    public Lote() {
-       
+    // Desconto em % por ingresso 
+    public String oferecerDesconto(Double desconto, String id) {
+        
+        String msg = "Ingresso não elegível a desconto";
+
+        for (int i = 0; i < ingressos.size(); i++) {
+            
+            if (ingressos.get(i).getId() == id && ingressos.get(i).getTipo() != TipoDeIngresso.MEIA) {
+                
+                if (desconto <= 25.0) {
+                    ingressos.get(i).setDesconto(desconto);
+                    msg = "Desconto de " + desconto + "%" + " aplicado";
+                } else {
+                    msg = "O desconto máximo é de apenas 25%";
+                }
+            }
+        }
+        return msg;
     }
+
+    // Aplicar desconto a todos os ingressos elegíveis do lote
+    public String oferecerDesconto(Double desconto) {
+        
+        String msg = "Não foi possível aplicar os descontos!";
+
+        for (int i = 0; i < ingressos.size(); i++) {
+            
+            if (ingressos.get(i).getTipo() != TipoDeIngresso.MEIA) {
+                
+                if (desconto <= 25.0) {
+                    ingressos.get(i).setDesconto(desconto);
+                    msg = "Desconto de " + desconto + "%" + " aplicado";
+                } else {
+                    msg = "O desconto máximo é de apenas 25%";
+                }
+            }
+        }
+        return msg;
+    }
+
 
     public String getId() {
         return id;
     }
-   
-    public List<Ingresso> getIngressos() {
-        return ingressos;
-    }
 
-    public void setIngressos(List<Ingresso> ingressos) {
-        this.ingressos = ingressos;
-    }
-
-    public Double getDesconto() {
-        return desconto;
-    }
-
-    public void setDesconto(Double desconto) {
-        this.desconto = desconto;
-    }
 
     public void setId(String id) {
         this.id = id;
     }
 
-    public void addIngresso(Ingresso ingresso) {
-        if (ingresso != null) {
-            ingressos.add(ingresso);
+
+    public List<Ingresso> getIngressos() {
+        return ingressos;
+    }
+
+
+    public void addIngressos(Ingresso ingresso) {
+        ingressos.add(ingresso);
+    }
+
+    public void removeIngressos(Ingresso ingresso) {
+        ingressos.remove(ingresso);
+    }
+
+    public Boolean getElegivelDesconto() {
+        return elegivelDesconto;
+    }
+
+
+    public void setElegivelDesconto(Boolean elegivelDesconto) {
+        this.elegivelDesconto = elegivelDesconto;
+    }
+
+    public double getIngressos_meia() {
+        return ingressos_meia;
+    }
+
+    public double getIngressos_vips() {
+        return ingressos_vips;
+    }
+
+    public double getIngressos_normais() {
+        return ingressos_normais;
+    }
+
+    public String cadastrarIngressos(int n_ingressos, double porcentagemVip, double valor_base) {
+        
+        String msg_ingressos = "";
+        if (porcentagemVip > maxIngressosVip) {
+            msg_ingressos = "Capacidade máxima de ingressos VIP é de 30%";
         } else {
-            throw new IllegalArgumentException("Ingresso não pode ser nulo");
+            ingressos_meia = n_ingressos * 0.10;
+            ingressos_vips = n_ingressos * (porcentagemVip/100);
+            ingressos_normais = n_ingressos - ingressos_vips - ingressos_meia;
+
+            for (int i = 0; i < ingressos_meia; i++) {
+                String id = i + "M";
+                Ingresso ingresso = new Ingresso(id, entities_enum.Status.DISPONIVEL, TipoDeIngresso.MEIA, valor_base / 2);
+                ingressos.add(ingresso);
+            }
+
+            for (int i = 0; i < ingressos_vips; i++) {
+                String id = i + "V";
+                Ingresso ingresso = new Ingresso(id, entities_enum.Status.DISPONIVEL, TipoDeIngresso.VIP, valor_base * 2);
+                ingressos.add(ingresso);
+            }
+
+            for (int i = 0; i < ingressos_normais; i++) {
+                String id = i + "N";
+                Ingresso ingresso = new Ingresso(id, entities_enum.Status.DISPONIVEL, TipoDeIngresso.NORMAL, valor_base);
+                ingressos.add(ingresso);
+            }
+
+            msg_ingressos = "Lote de ingressos cadastrados com sucesso!";
         }
+
+        return msg_ingressos;
     }
 
-    public double calcularValorComDesconto(double valorOriginal) {
-        return valorOriginal - valorOriginal * desconto;
+    public boolean verificaDisponibilidade(String tipo) {
+        for (int i = 0; i < ingressos.size(); i++) {
+            if (ingressos.get(i).getTipo().toString().equals(tipo.toUpperCase()) && ingressos.get(i).getStatus().equals(Status.DISPONIVEL)) {
+                return true;
+            }
+        }
+        return false;
     }
-
 }
