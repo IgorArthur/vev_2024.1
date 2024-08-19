33cd7a340d3a43bbb1ca1aba54e54a9f63a6ab12
LuizCarlosXavier <luizcarlosabrantes01@gmail.com>
2024-08-19 17:04:39 -0300

    TS: Testes por partição de equivalência para a classe Show.java

--- src/test/java/ShowTest.java
+++ src/test/java/ShowTest.java
@@ -5,74 +5,75 @@ import entities.Lote;
 import entities.Show;
 
 public class ShowTest {
-     @Test
-    public void vendaDeIngressosVipLimiteSuperior() {
+
+    @Test
+    public void vendaDeIngressosVipParticaoValida() {
         Lote lote = new Lote("lote02");
         lote.cadastrarIngressos(500, 20, 10.0);
         Show show = new Show("08/05/2025", "GL", 1000.0, 1500.0, false);
         show.addLote(lote);
 
-        // Venda de ingressos até o limite superior da quantidade, dado o tipo de um ingresso
+        // Venda de ingressos dentro da partição válida para VIP
         show.venderIngresso("vip", 100);
         Assert.assertEquals(100, show.getVipsVendidos());
     }
 
     @Test
-    public void vendaDeIngressosVipAcimaLimiteSuperior() {
+    public void vendaDeIngressosVipParticaoInvalida() {
         Lote lote = new Lote("lote02");
         lote.cadastrarIngressos(500, 20, 10.0);
         Show show = new Show("08/05/2025", "GL", 1000.0, 1500.0, false);
         show.addLote(lote);
 
-        // Venda de ingressos acima do limite superior da quantidade, dado o tipo de um ingresso
+        // Venda de ingressos fora da partição válida para VIP
         show.venderIngresso("vip", 101);
         Assert.assertEquals(0, show.getVipsVendidos());
     }
 
     @Test
-    public void vendaDeIngressosNormalLimiteSuperior() {
+    public void vendaDeIngressosNormalParticaoValida() {
         Lote lote = new Lote("lote02");
         lote.cadastrarIngressos(500, 20, 10.0);
         Show show = new Show("08/05/2025", "GL", 1000.0, 1500.0, false);
         show.addLote(lote);
 
-        // Venda de ingressos até o limite superior da quantidade restante para os ingressos normais
+        // Venda de ingressos dentro da partição válida para normal
         show.venderIngresso("normal", 350);
         Assert.assertEquals(350, show.getNormaisVendidos());
     }
 
     @Test
-    public void vendaDeIngressosNormalAcimaDoLimiteSuperior() {
+    public void vendaDeIngressosNormalParticaoInvalida() {
         Lote lote = new Lote("lote02");
         lote.cadastrarIngressos(500, 20, 10.0);
         Show show = new Show("08/05/2025", "GL", 1000.0, 1500.0, false);
         show.addLote(lote);
 
-        // Venda de ingressos até o limite superior da quantidade restante para os ingressos normais
+        // Venda de ingressos fora da partição válida para normal
         show.venderIngresso("normal", 351);
         Assert.assertEquals(0, show.getNormaisVendidos());
     }
 
     @Test
-    public void vendaDeIngressosMeiaLimiteSuperior() {
+    public void vendaDeIngressosMeiaParticaoValida() {
         Lote lote = new Lote("lote02");
         lote.cadastrarIngressos(500, 20, 10.0);
         Show show = new Show("08/05/2025", "GL", 1000.0, 1500.0, false);
         show.addLote(lote);
 
-        // Venda de ingressos até o limite superior da quantidade restante para os ingressos normais
+        // Venda de ingressos dentro da partição válida para meia
         show.venderIngresso("meia", 50);
         Assert.assertEquals(50, show.getMeiasVendidos());
     }
 
     @Test
-    public void vendaDeIngressosMeiaAcimaDoLimiteSuperior() {
+    public void vendaDeIngressosMeiaParticaoInvalida() {
         Lote lote = new Lote("lote02");
         lote.cadastrarIngressos(500, 20, 10.0);
         Show show = new Show("08/05/2025", "GL", 1000.0, 1500.0, false);
         show.addLote(lote);
 
-        // Venda de ingressos até o limite superior da quantidade restante para os ingressos normais
+        // Venda de ingressos fora da partição válida para meia
         show.venderIngresso("meia", 51);
         Assert.assertEquals(0, show.getMeiasVendidos());
     }
